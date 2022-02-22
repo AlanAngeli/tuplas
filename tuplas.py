c652b7f2cd5f4fc893d6b549b2c74b83bdc384ef
Alan <71236097+AlanAngeli@users.noreply.github.com>
2022-02-22 18:12:35 -0300

    Add files via upload

---
+++ tuplas.py
@@ -0,0 +1,13 @@
+tupla = (1,2,3,"a", "Alan Angeli")
+
+print(tupla)#mostra toda a tupla
+print()
+print(tupla[4]) #mostra o 5 índice da tupla
+print()
+
+for all in tupla:
+    print(all)
+
+print()
+
+print(tupla[3:5])
